3a5778b8fc30171678395154150b290053baab32
Nayab <nayabnaveed046>
2020-11-25 23:27:02 -0800

    Start BMI

---
+++ lib/constent_variable.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const activationcolor = Colors.cyan;
+const deactivationcolor = Color(0xFF1D1E33);
+
+const ConstentTextLabel = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.bold,
+);
+
+const ConstentTextLabe2 = TextStyle(
+  fontSize: 35,
+  fontWeight: FontWeight.w900,
+);
+
+const ConstentTextLabe3 = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w900,
+);
+
+const Result1 = TextStyle(
+  color: Colors.greenAccent,
+  fontSize: 20.0,
+);
+const Result2 = TextStyle(
+  color: Colors.white,
+  fontSize: 80.0,
+  fontWeight: FontWeight.w900,
+);
---
+++ lib/main.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'package:bmi_calculator/MainPage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'MainPage.dart';
+
+
+void main() => runApp(SplashScreen());
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MySplashScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+
+class MySplashScreen extends StatefulWidget {
+  @override
+  _MySplashScreenState createState() => _MySplashScreenState();
+}
+
+class _MySplashScreenState extends State<MySplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 5), ()=>Navigator.push(context, MaterialPageRoute(builder: (context) => BmiCalculatur())));
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Image.asset('images/icon.png', height: 200.0, width: 200,),
+
+          SizedBox( height: 20.0),
+
+          SpinKitRipple(color: Colors.blueGrey, size: 150.0, borderWidth: 10.0,),
+        ],
+      ),
+    );
+  }
+}
